d3e210f1dd3541094584be7a0f2e8f43c43ccc84
kmskhn <khnkms@gmail.com>
2025-10-19 20:45:22 +0530

    completed all functionalities

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -13,54 +13,86 @@ import { useEffect, useState } from "react";
 const options = [
   {
     label: "First Name",
-    Value: "first_name",
+    value: "first_name",
   },
   {
     label: "Last Name",
-    Value: "last_name",
+    value: "last_name",
   },
   {
     label: "Gender",
-    Value: "gender",
+    value: "gender",
   },
   {
     label: "Age",
-    Value: "age",
+    value: "age",
   },
   {
     label: "Account Name",
-    Value: "account_name",
+    value: "account_name",
   },
   {
     label: "City",
-    Value: "city",
+    value: "city",
   },
   {
     label: "State",
-    Value: "state",
+    value: "state",
   },
 ];
 
 export default function DialogDemo() {
   const [selectedSchema, setSelectedSchema] = useState({});
   const [selectedSchemasList, setSelectedSchemasList] = useState([]);
+  const [unselectedOptions, setUnselectedOptions] = useState(options);
+  const [selectValue, setSelectValue] = useState("");
+  const [segmentName, setSegmentName] = useState("");
 
   function handleSelectChange(e) {
     const value = e.target.value;
-    const selectedItem = options.find((opt) => opt.Value === value);
-    console.log(selectedItem.Value);
+    const selectedItem = options.find((opt) => opt.value === value);
+    setSelectedSchema(selectedItem);
+    setSelectValue(value);
   }
 
   function addSchema() {
-    if (selectedSchema && selectedSchema.Value) {
-      setSelectedSchemasList(...selectedSchemasList, selectedSchema);
+    if (selectedSchema && selectedSchema.value) {
+      setSelectedSchemasList([...selectedSchemasList, selectedSchema]);
       setSelectedSchema({});
+      setUnselectedOptions((prev) =>
+        prev.filter((opt) => opt.value !== selectedSchema.value)
+      );
+      setSelectValue("");
     }
   }
 
-  useEffect(() => {
-    console.log("selectedSchemasList", selectedSchemasList);
-  }, [selectedSchemasList]);
+  function removeSchema(value) {
+    setSelectedSchemasList((prev) =>
+      prev.filter((item) => item.value !== value)
+    );
+    setUnselectedOptions((prev) => {
+      const removedItem = selectedSchemasList.find(
+        (item) => item.value === value
+      );
+      return [...prev, removedItem].sort((a, b) =>
+        a.label.localeCompare(b.label)
+      );
+    });
+  }
+
+  function sentData(){
+    fetch('https://webhook.site/c99f5702-b2b2-41a2-a45b-f9d333ca701a', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        segment_name: segmentName,
+        schema: selectedSchemasList,
+      }),
+    })
+  }
+
   return (
     <Dialog>
       <form className="h-[100vh] bg-gradient-to-b from-gray-500 via-gray-400 to-gray-100 p-0">
@@ -85,21 +117,52 @@ export default function DialogDemo() {
               type="text"
               placeholder="Name of the Segment"
               className="border w-full p-2 rounded-sm"
+              value={segmentName}
+              onChange={(e) => setSegmentName(e.target.value)}
             />
           </div>
           <p>
             To save your segment, you need to add the schema to build your query
           </p>
-          <div className="border border-blue-700">
+          <div
+            className={` ${
+              selectedSchemasList.length > 0
+                ? "border border-blue-700  px-2 py-4"
+                : ""
+            } flex flex-col gap-2`}
+          >
             {selectedSchemasList.map((item, index) => {
-              return <p>{item.Value}</p>;
+              return (
+                <div className="flex gap-2">
+                  <select className="border py-2 px-1 flex-1" key={index}>
+                    <option value={item.value}>{item.label}</option>
+                    {unselectedOptions.map((opt, idx) => {
+                      return (
+                        <option value={opt.value} key={idx}>
+                          {opt.label}
+                        </option>
+                      );
+                    })}
+                  </select>
+                  <button
+                    onClick={() => removeSchema(item.value)}
+                    className="bg-gray-200 text-gray-600 px-5 py-1 rounded hover:bg-gray-300"
+                  >
+                    -
+                  </button>
+                </div>
+              );
             })}
           </div>
-          <select className="border p-2" onChange={handleSelectChange}>
-            <option>Add schema to the segment</option>
-            {options.map((item, index) => {
+          <select
+            className="border p-2"
+            onChange={handleSelectChange}
+            value={selectValue}
+          >
+            <option value="">Add schema to the segment</option>
+            {unselectedOptions.map((item, index) => {
               return (
-                <option value={item.Value} key={index}>
+                <option value={item.value} key={index}>
                   {item.label}
                 </option>
               );
@@ -117,9 +180,8 @@ export default function DialogDemo() {
           </button>
           <DialogFooter className="sm:justify-start">
             <DialogClose asChild>
-              <Button variant="outline">Save the Segment</Button>
+              <Button variant="outline" className="bg-[#0ca678] text-white" onClick={sentData}>Save the Segment</Button>
             </DialogClose>
-            <Button type="submit">Cancel</Button>
           </DialogFooter>{" "}
         </DialogContent>
       </form>
